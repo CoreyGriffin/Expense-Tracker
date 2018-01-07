92a7de7572538b8f3b15dd32a2d1799e95c5dc1e
Corey Griffin <coreyjgriff@gmail.com>
2018-01-07 08:50:47 -0500

    remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,15 +12,7 @@ import './styles/styles.scss';
 const reduxStore = store();
 
 reduxStore.subscribe(() => {
-  const state = reduxStore.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
 })
-
-reduxStore.dispatch(addExpense({description: 'Water Bill', amount: '3000', createdAt: '2017'}))
-reduxStore.dispatch(addExpense({description: 'Gas Bill', amount: '2000', createdAt: '2017'}))
-reduxStore.dispatch(addExpense({description: 'Cable Bill', amount: '50000', createdAt: '2017'}))
-// reduxStore.dispatch(setTextFilter('water'));
 
 const jsx = (
   <Provider store={reduxStore}>
